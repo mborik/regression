90420fd769296034422cb4cec928917247f6d32b
mborik128 <mborik@users.sourceforge.net>
2019-02-24 19:32:15 +0100

    go!go!go! - microkernel for snapshot-test of parts - improved kernel with ldir blocks - fixed glitch in oneplus data - draft of oneplus fx - reglogo square fixed

--- builder.cmd
+++ builder.cmd
@@ -3,13 +3,20 @@ mbdnew regression.mbd 82 11 "REGRESSION demo by NAG/svk"
 @cd kernel
 @cd ..\cityflyout
 del /Q /F final.pak
-sjasmplus --lst=cityflyout.lst cityflyout.a80
+sjasmplus -Dfx --lst=cityflyout.lst cityflyout.a80
 lzxpack -t36o7o14 final.bin
 ren final-t36o7o14.lzx final.pak
 
 @cd ..\reglogo
 del /Q /F final.pak
-sjasmplus --lst=reglogo.lst reglogo.a80
+sjasmplus -Dfx --lst=reglogo.lst reglogo.a80
+lzxpack -t36o7o14 final.bin
+ren final-t36o7o14.lzx final.pak
+
+@cd ..\oneplus
+del /Q /F final.pak
+sjasmplus oneplusani.a80
+sjasmplus -Dfx --lst=oneplus.lst oneplus.a80
 lzxpack -t36o7o14 final.bin
 ren final-t36o7o14.lzx final.pak
 
@@ -19,6 +26,10 @@ del /Q /F output needle*
 sjasmplus pg1fx.a80 --exp=kernel/pg1fx.inc
 lzxpack -t36o7o14 output
 ren output-t36o7o14.lzx needle1
+
+sjasmplus pg3fx.a80 --exp=kernel/pg3fx.inc
+lzxpack -t36o7o14 output
+ren output-t36o7o14.lzx needle3
 
 sjasmplus pg6fx.a80 --exp=kernel/pg6fx.inc
 lzxpack -t36o7o14 output
@@ -31,6 +42,7 @@ tap2mbd REGRESSION.tap 0 "..\regression.mbd"
 @cd ..
 bin2mbd haystack -a 0 -o "regression.mbd"
 bin2mbd needle1 -a 49152 -o "regression.mbd"
+bin2mbd needle3 -a 49152 -o "regression.mbd"
 bin2mbd needle6 -a 49152 -o "regression.mbd"
 
 @rem CLEANUP!
