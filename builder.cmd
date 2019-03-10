24e99df9d250e1af651d1a483be055ce2008da29
mborik128 <mborik@users.sourceforge.net>
2019-03-11 00:41:16 +0100

    page 4, music changed - oneplus & surprise bugfixes

--- builder.cmd
+++ builder.cmd
@@ -26,18 +26,28 @@ sjasmplus -Dfx --lst=surprise.lst surprise.a80
 lzxpack -t36o7o14 final.bin
 ren final-t36o7o14.lzx final.pak
 
+@cd ..\teleso
+del /Q /F final.pak
+sjasmplus -Dfx --lst=teleso.lst teleso.a80
+lzxpack -t36o7o14 final.bin
+ren final-t36o7o14.lzx final.pak
+
 @cd ..
 del /Q /F output needle*
 
-sjasmplus pg1fx.a80 --exp=kernel/pg1fx.inc
+sjasmplus pg1fx.a80 --lst=kernel/pg1fx.lst --exp=kernel/pg1fx.inc
 lzxpack -t36o7o14 output
 ren output-t36o7o14.lzx needle1
 
-sjasmplus pg3fx.a80 --exp=kernel/pg3fx.inc
+sjasmplus pg3fx.a80 --lst=kernel/pg3fx.lst --exp=kernel/pg3fx.inc
 lzxpack -t36o7o14 output
 ren output-t36o7o14.lzx needle3
 
-sjasmplus pg6fx.a80 --exp=kernel/pg6fx.inc
+sjasmplus pg4fx.a80 --lst=kernel/pg4fx.lst --exp=kernel/pg4fx.inc
+lzxpack -t36o7o14 output
+ren output-t36o7o14.lzx needle4
+
+sjasmplus pg6fx.a80 --lst=kernel/pg6fx.lst --exp=kernel/pg6fx.inc
 lzxpack -t36o7o14 output
 ren output-t36o7o14.lzx needle6
 
@@ -49,6 +59,7 @@ tap2mbd REGRESSION.tap 0 "..\regression.mbd"
 bin2mbd haystack -a 0 -o "regression.mbd"
 bin2mbd needle1 -a 49152 -o "regression.mbd"
 bin2mbd needle3 -a 49152 -o "regression.mbd"
+bin2mbd needle4 -a 49152 -o "regression.mbd"
 bin2mbd needle6 -a 49152 -o "regression.mbd"
 
 @rem CLEANUP!
